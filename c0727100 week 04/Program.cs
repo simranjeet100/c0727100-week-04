637c4b8e44edb855ae225f6a1ae1156d65320cc9
visualstudio <visualstudio@122-PC13>
2019-02-01 21:58:23 -0500

    SIMRAN

--- c0727100 week 04/Program.cs
+++ c0727100 week 04/Program.cs
@@ -4,12 +4,94 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace c0727100_week_04
+namespace C099999
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Countryside c = new Countryside();
+            c.run();
+            c.travel();
+            Console.ReadLine();
+
         }
     }
+
+    class LearningExperiment
+    {
+        public void run()
+        {
+            Village Toronto;
+            Toronto = new Village();
+            Village a, b, c;
+            Toronto.villageName = "version A";
+            a = Toronto;
+            Console.WriteLine(a.villageName);
+            Toronto = new Village();
+            Toronto.villageName = "version B";
+            b = Toronto;
+            Console.WriteLine(b.villageName);
+            Toronto = new Village();
+            Toronto.villageName = "version C";
+            c = Toronto;
+            Console.WriteLine(c.villageName);
+
+
+        }
+    }
+
+
+    class Village
+    {
+        public bool isAstrildeHere;
+        public Village nextVillage;
+        public Village prevVillage;
+        public string villageName;
+    }
+
+    class Countryside
+    {
+        Village Toronto, Maple, Ajax, CurrentVillage;
+
+        public void run()
+        {
+            Maple = new Village();
+            Toronto = new Village();
+            Ajax = new Village();
+            Ajax.villageName = "Ajax";
+            Ajax.isAstrildeHere = true;
+            Maple.villageName = "Maple";
+            Maple.nextVillage = Toronto;
+            Toronto.nextVillage = Ajax;
+            Toronto.villageName = "Toronto";
+            Ajax.nextVillage = null;
+        }
+        public void travel()
+        {
+            CurrentVillage = Maple;
+
+            while (true)
+            {
+                if (CurrentVillage.isAstrildeHere)
+                {
+                    Console.WriteLine("Astrilde is in " + CurrentVillage.villageName);
+                    Console.ReadLine();
+                }
+                else { CurrentVillage = CurrentVillage.nextVillage; }
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+
 }
